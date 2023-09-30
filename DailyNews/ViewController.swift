839ae677a4d9a8316754b83086c41cc979e90c46
bahittinn <bahittin84@gmail.com>
2023-09-30 22:48:00 +0300

    Initial Commit

---
+++ DailyNews/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  DailyNews
+//
+//  Created by Bahittin on 30.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+}
+
