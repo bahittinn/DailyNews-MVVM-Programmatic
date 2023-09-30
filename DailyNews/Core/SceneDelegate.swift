cc53cd99d27681ae04a89286fcb9beac0f550f91
bahittinn <bahittin84@gmail.com>
2023-09-30 22:53:17 +0300

    project setup

---
+++ DailyNews/Core/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  DailyNews
+//
+//  Created by Bahittin on 30.09.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let vc = UINavigationController(rootViewController: ViewController())
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController =  vc
+        self.window = window
+        self.window?.makeKeyAndVisible()
+    }
+}
+
