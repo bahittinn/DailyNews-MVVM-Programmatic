c4d5f5953db5085615fc4ef86a9b9d1eb957b464
bahittinn <bahittin84@gmail.com>
2023-10-30 12:56:29 +0300

    collectionview Setup

--- DailyNews/Controllers/HomeController.swift
+++ DailyNews/Controllers/HomeController.swift
@@ -7,12 +7,59 @@
 
 import UIKit
 
-class HomeController: UIViewController {
+final class HomeController: UIViewController {
     
+    //MARK: - Variables
+    
+    public var screenWidth: CGFloat {
+        return UIScreen.main.bounds.width
+    }
+    
+    // Screen height.
+    public var screenHeight: CGFloat {
+        return UIScreen.main.bounds.height
+    }
+    
+    private let trendingCollectionView: UICollectionView = {
+        /// Layout Settings
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let itemWidth = UIScreen.main.bounds.size.width * 0.8
+        layout.itemSize = CGSize(width: itemWidth, height: 250)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "CollectionCell")
+        collectionView.showsHorizontalScrollIndicator = false
+        return collectionView
+    }()
+    
+    //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         
+        configureNavigationBar()
+        configureContraints()
+    }
+    
+    //MARK: - Constraints
+    func configureContraints() {
+        view.addSubview(trendingCollectionView)
+        trendingCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        trendingCollectionView.delegate   = self
+        trendingCollectionView.dataSource = self
+        
+        NSLayoutConstraint.activate([
+            trendingCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            trendingCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            trendingCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            trendingCollectionView.heightAnchor.constraint(equalToConstant: 260)
+        ])
+    }
+    
+    //MARK: - Functions
+    
+    private func configureNavigationBar() {
         /// LEFT BUTTONS
         let menuButton = UIBarButtonItem(title: "Menu", image: UIImage(named: "menuicon"), target: self, action: nil)
         menuButton.tintColor = .black
@@ -31,6 +78,18 @@ class HomeController: UIViewController {
         searchButton.tintColor = .black
                 
         navigationItem.rightBarButtonItems = [bellButton, searchButton]
-        
+    }
+}
+
+extension HomeController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionCell", for: indexPath)
+        cell.backgroundColor = .red
+        cell.layer.cornerRadius = 10
+        return cell
     }
 }
