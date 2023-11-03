645be03a4ca5c1358e7445b4895bd4db152b9636
bahittinn <bahittin84@gmail.com>
2023-11-03 23:34:07 +0300

    popup ui design

--- DailyNews/Utils/CustomViews/ViewController/NewsAlertVC.swift
+++ DailyNews/Utils/CustomViews/ViewController/NewsAlertVC.swift
@@ -7,33 +7,58 @@
 
 import UIKit
 
-class NewsAlertVC: UIViewController {
+final class NewsAlertVC: UIViewController {
     
     private let denemeButton: UIButton = {
         let button = UIButton()
-        button.setTitle("Deneme", for: .normal)
+        button.layer.cornerRadius    = 10
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("OK", for: .normal)
         button.addTarget(self, action: #selector(dismissVC), for: .touchUpInside)
         return button
     }()
+    
+    private let containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemBackground
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 10
+        return view
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .black.withAlphaComponent(0.75)
+        configureContainerView()
         configureButton()
     }
     
+    func configureContainerView() {
+        view.addSubview(containerView)
+        
+        NSLayoutConstraint.activate([
+            containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            containerView.widthAnchor.constraint(equalToConstant: 280),
+            containerView.heightAnchor.constraint(equalToConstant: 220)
+        ])
+    }
+    
     func configureButton() {
-        view.addSubview(denemeButton)
+        containerView.addSubview(denemeButton)
         denemeButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            denemeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            denemeButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            denemeButton.widthAnchor.constraint(equalToConstant: 100),
+            denemeButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -10),
+            denemeButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 10),
+            denemeButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -10),
             denemeButton.heightAnchor.constraint(equalToConstant: 35)
         ])
     }
+    
     
     @objc func dismissVC() {
         dismiss(animated: true)
